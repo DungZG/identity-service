20608f0f813d2b03d007bbbae24fe2fd4ee61cd3
DungZG <dungzg1201@gmail.com>
2024-11-17 16:03:06 +0700

    Update

--- src/main/java/com/devteria/identity_service/dto/Request/UserCreationRequest.java
+++ src/main/java/com/devteria/identity_service/dto/Request/UserCreationRequest.java
@@ -1,9 +1,13 @@
 package com.devteria.identity_service.dto.Request;
 
 import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDate;
 
+@Getter
+@Setter
 public class UserCreationRequest {
     @Size(min = 3, message ="USERNAME_INVALID")
     private String username;
@@ -12,46 +16,45 @@ public class UserCreationRequest {
     private String password;
     private String firstName;
     private String lastName;
-
     private LocalDate dod;
 
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public LocalDate getDod() {
-        return dod;
-    }
-
-    public void setDod(LocalDate dod) {
-        this.dod = dod;
-    }
+//    public String getUsername() {
+//        return username;
+//    }
+//
+//    public void setUsername(String username) {
+//        this.username = username;
+//    }
+//
+//    public String getPassword() {
+//        return password;
+//    }
+//
+//    public void setPassword(String password) {
+//        this.password = password;
+//    }
+//
+//    public String getFirstName() {
+//        return firstName;
+//    }
+//
+//    public void setFirstName(String firstName) {
+//        this.firstName = firstName;
+//    }
+//
+//    public String getLastName() {
+//        return lastName;
+//    }
+//
+//    public void setLastName(String lastName) {
+//        this.lastName = lastName;
+//    }
+//
+//    public LocalDate getDod() {
+//        return dod;
+//    }
+//
+//    public void setDod(LocalDate dod) {
+//        this.dod = dod;
+//    }
 }
--- src/main/java/com/devteria/identity_service/dto/Request/UserUpdateRequest.java
+++ src/main/java/com/devteria/identity_service/dto/Request/UserUpdateRequest.java
@@ -1,7 +1,12 @@
 package com.devteria.identity_service.dto.Request;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.time.LocalDate;
 
+@Getter
+@Setter
 public class UserUpdateRequest {
     private String password;
     private String firstName;
@@ -9,35 +14,35 @@ public class UserUpdateRequest {
     private LocalDate dod;
 
 
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public LocalDate getDod() {
-        return dod;
-    }
-
-    public void setDod(LocalDate dod) {
-        this.dod = dod;
-    }
+//    public String getPassword() {
+//        return password;
+//    }
+//
+//    public void setPassword(String password) {
+//        this.password = password;
+//    }
+//
+//    public String getFirstName() {
+//        return firstName;
+//    }
+//
+//    public void setFirstName(String firstName) {
+//        this.firstName = firstName;
+//    }
+//
+//    public String getLastName() {
+//        return lastName;
+//    }
+//
+//    public void setLastName(String lastName) {
+//        this.lastName = lastName;
+//    }
+//
+//    public LocalDate getDod() {
+//        return dod;
+//    }
+//
+//    public void setDod(LocalDate dod) {
+//        this.dod = dod;
+//    }
 }
