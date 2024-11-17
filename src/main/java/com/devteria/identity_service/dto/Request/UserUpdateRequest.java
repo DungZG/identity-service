de9b59017f44a21f5bdc11f64dfd063e73698e7e
DungZG <dungzg1201@gmail.com>
2024-11-17 18:04:09 +0700

    Update

--- src/main/java/com/devteria/identity_service/dto/Request/UserUpdateRequest.java
+++ src/main/java/com/devteria/identity_service/dto/Request/UserUpdateRequest.java
@@ -16,36 +16,4 @@ public class UserUpdateRequest {
     String lastName;
     LocalDate dod;
 
-
-//    public String getPassword() {
-//        return password;
-//    }
-//
-//    public void setPassword(String password) {
-//        this.password = password;
-//    }
-//
-//    public String getFirstName() {
-//        return firstName;
-//    }
-//
-//    public void setFirstName(String firstName) {
-//        this.firstName = firstName;
-//    }
-//
-//    public String getLastName() {
-//        return lastName;
-//    }
-//
-//    public void setLastName(String lastName) {
-//        this.lastName = lastName;
-//    }
-//
-//    public LocalDate getDod() {
-//        return dod;
-//    }
-//
-//    public void setDod(LocalDate dod) {
-//        this.dod = dod;
-//    }
 }
